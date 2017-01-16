10891c3472974b675acb0633b9c4a8e1c87e0fa0
LongoriaYou <LongoriaYou@tutorabc.com>
2017-01-16 19:51:30 +0800

    the first  quartz example

---
+++ QuartzLearning/Quartz/Jobs/HelloJob.cs
@@ -0,0 +1,38 @@
+﻿using Quartz;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuartzLearning.Quartz.Jobs
+{
+    [DisallowConcurrentExecution]
+    [PersistJobDataAfterExecution]
+    public class HelloJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                string now = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+                JobKey key = context.JobDetail.Key;
+                JobDataMap dataMap = context.JobDetail.JobDataMap;
+
+                string lastExecutionTime = dataMap.GetString("LastExecutionTime");
+                string userName = dataMap.GetString("UserName");
+                
+                Debug.WriteLine($"Instance {key}: Current Time is {now} and the last execution time is {lastExecutionTime} HelloJob is executing by {userName}.");
+
+                // update JobDataMap by replacing the original job with the given name and group
+                //dataMap["LastExecutionTime"] = now;
+                dataMap.Put("LastExecutionTime", now);
+            }
+            catch (JobExecutionException ex)
+            {
+                Debug.WriteLine($"Error Message: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ QuartzLearning/Quartz/SchedulerFactory.cs
@@ -0,0 +1,41 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using QuartzLearning.Quartz.Jobs;
+
+namespace QuartzLearning.Quartz
+{
+    public class SchedulerFactory
+    {
+        public static void CreateSchedule()
+        {
+            ISchedulerFactory schedFact = new StdSchedulerFactory();
+
+            // get a scheduler
+            IScheduler sched = schedFact.GetScheduler();
+            sched.Start();
+
+            // define the job and tie it to our HelloJob class
+            IJobDetail job = JobBuilder.Create<HelloJob>()
+               .UsingJobData("LastExecutionTime", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"))
+               .UsingJobData("UserName", "Longoria")
+               .WithIdentity("myJob", "group1")
+               .Build();
+
+            // Trigger the job to run now, and then every 5 seconds
+            ITrigger trigger = TriggerBuilder.Create()
+              .WithIdentity("myTrigger", "group1")
+              //.ForJob(job)
+              .StartNow()
+              .WithSimpleSchedule(x => x
+                  .WithIntervalInSeconds(5)
+                  .RepeatForever())
+              .Build();
+
+            sched.ScheduleJob(job, trigger);
+        }
+    }
+}
